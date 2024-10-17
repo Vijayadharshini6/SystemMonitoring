e5b5eea2d9c08bca52e955a6b43c4042ac53e761
Vijayadharshini6 <149487301+Vijayadharshini6@users.noreply.github.com>
2024-10-17 14:41:03 +0530

    Add files via upload

---
+++ system/ClientServlet.java
@@ -0,0 +1,50 @@
+package system;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+import com.google.gson.JsonObject;
+
+
+@WebServlet("/ClientServlet")
+public class ClientServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+   
+    public ClientServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {	
+		
+		 response.setContentType("text/plain");
+	        
+	        // Get PrintWriter to write response
+	        PrintWriter writer = response.getWriter();
+	        
+	        // Write response
+	        writer.print("hello");
+		 // response.setContentType("application/json");
+		String parameter = request.getParameter("table");
+		 int int1 = Integer.parseInt(request.getParameter("offset"));
+		 int int2 = Integer.parseInt(request.getParameter("limit"));
+		 
+		List<JsonObject> dataWithLimitAndOffset = DBFunction.getDataWithLimitAndOffset(parameter, int2, int1);
+		 writer.println(dataWithLimitAndOffset.toString());
+		 	
+		
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+	}
+
+}
